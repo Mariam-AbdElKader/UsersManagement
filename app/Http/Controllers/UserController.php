2e181d956290ef3276ba3315239c8dc6ad945246
Mariam-AbdElKader <mariamabdelkader62@gmail.com>
2025-12-13 13:46:02 +0200

    Fix: Correct casing of User model import in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Client\UpdateUserRequest;
-use App\Models\user;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
